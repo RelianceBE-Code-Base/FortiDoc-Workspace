57ad118999ff652a94d7d2a14ca00c7734176c1c
Reliance BE <136088232+RelianceBE-Code-Base@users.noreply.github.com>
2024-06-01 20:25:51 +0000

    Removed ComponentsGrid components from main digital workspace webpart components

--- src/webparts/digitalWorkspace/components/UserProfile/IUserProfileProps.ts
+++
@@ -1,7 +0,0 @@
-import { WebPartContext } from "@microsoft/sp-webpart-base";
-
-export interface IUserProfileProps {
-  context: WebPartContext;
-  isDarkTheme: boolean;
-  userDisplayName: string;
-}
