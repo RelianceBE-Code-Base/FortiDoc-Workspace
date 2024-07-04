138cf0d63954184a900251bae1e0421735d176d0
Reliance BE <136088232+RelianceBE-Code-Base@users.noreply.github.com>
2024-07-04 23:56:40 +0000

    Modified UserProfile component to fetch logged in user's data

--- src/webparts/ComponentsGrid/components/UserProfile/UserProfile.tsx
+++ src/webparts/ComponentsGrid/components/UserProfile/UserProfile.tsx
@@ -1,90 +1,74 @@
-import * as React from 'react';
-
+import React, { useState, useEffect } from 'react';
 import styles from './UserProfile.module.scss';
-
 import { IUserProfileProps } from './IUserProfileProps';
+import { ResponseType } from '@microsoft/microsoft-graph-client';
+// import { TestImages } from '@fluentui/example-data';
 
-import { IUserProfileState } from './IUserProfileState';
+const UserProfileIcon = require('./assets/UserProfile.png');
 
-import { TestImages } from '@fluentui/example-data';
+const UserProfile: React.FC<IUserProfileProps> = (props) => {
+  const [profilePhoto, setProfilePhoto] = useState<string>('');
+  const [userName, setUserName] = useState<string>('');
+  const [userJobTitle, setUserJobTitle] = useState<string>('');
 
-const UserProfileIcon = require('./assets/UserProfile.png')
+  const getProfilePhoto = async () => {
+    try {
+      const response = await props.graphClient
+        .api('me/photo/$value')
+        .responseType(ResponseType.BLOB)
+        .get();
 
-class UserProfile extends React.Component<IUserProfileProps, IUserProfileState> {
-
-  constructor(props: IUserProfileProps){
-    super(props)
-    this.state = {
-      profilePhoto: ""
+      const imageUrl = URL.createObjectURL(response);
+      setProfilePhoto(imageUrl);
+    } catch (error) {
+      console.log("Image not found");
+      setProfilePhoto("https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png");
     }
-  }
+  };
+  
 
-  getProfilePhoto = async () => {
-    try{
-    const response = await this.props.graphClient
-    .api('me/photo/$value')
-    .get()
-
-    this.setState({profilePhoto: response})
+  const getUserDetails = async () => {
+    try {
+      const response = await props.graphClient.api('/me').get();
+      setUserName(response.displayName);
+      setUserJobTitle(response.jobTitle);
+    } catch (error) {
+      console.log("User details not found");
     }
-    catch(error){
-      console.log("Image not found")
-      this.setState({profilePhoto: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"})
-    }
-  }
+  };
 
-  // loadProfile = async () => {
+  useEffect(() => {
+    // Uncomment the following lines to fetch the profile photo and user details from the Graph API
+    getProfilePhoto();
+    getUserDetails();
 
-  //   try {
-  //     const response = await this.props.graphClient
-  //     .api('/me')
-  //   }
+    // Set default values for testing purposes
+    // setProfilePhoto(TestImages.personaMale);
+    // setUserName("Toba Gbeleyi");
+    // setUserJobTitle("Developer");
+  }, []);
 
-  // }
-
-
-  componentDidMount(): void {
-
-    // this.getProfilePhoto();
-
-    // this.setState({profilePhoto: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"})
-    this.setState({profilePhoto: TestImages.personaMale})
-    
-  }
-
-
- 
-
-  render(): React.ReactNode {
-    return(
-      <div className={styles.card}>
-        <div className={styles['card-header']}>
-          <img src={UserProfileIcon}/>
-          <p style={{display: 'flex'}}>My Profile</p>
-          <div></div>
-       </div>
-
-
-
-       <div className={styles['card-body']}>
-         <div className= {styles['profile-image-frame']}>
-            <img className= {styles['profile-image']}  src={this.state.profilePhoto} alt='Profile Photo' />
-         </div>
-         <div className={styles.details}>
-          <div className={styles.title}>Toba Gbeleyi</div>
-          <div className={styles.subtitle}>Developer</div>
-         </div>
-
-          <div className="contact">
-            <span></span>
-          </div>
-
-         </div>
-       </div>
-    )
-  }
-
-
+  return (
+    <div className={styles.card}>
+      <div className={styles['card-header']}>
+        <img src={UserProfileIcon} alt="User Profile Icon"/>
+        <p style={{ display: 'flex' }}>My Profile</p>
+        <div></div>
+      </div>
+      <div className={styles['card-body']}>
+        <div className={styles['profile-image-frame']}>
+          <img className={styles['profile-image']} src={profilePhoto} alt='Profile Photo' />
+        </div>
+        <div className={styles.details}>
+          <div className={styles.title}>{userName}</div>
+          <div className={styles.subtitle}>{userJobTitle}</div>
+        </div>
+        <div className="contact">
+          <span></span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserProfile;
